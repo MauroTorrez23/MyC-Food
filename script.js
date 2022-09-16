96ab23b0dcc60bab02690912e260a4085afe9018
unknown <mauro.torrez.97@gmail.com>
2022-09-16 13:39:06 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//home carousel
+$('.home-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    dots: true,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
